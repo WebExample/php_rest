d11a52a86ee501f2e997cc6414eb9de8c3e80a3a
ken <kenngsimply@gmail.com>
2014-07-07 11:14:32 +0800

    php_rest_example_by_Ludovico_Fischer

---
+++ php_rest_example_by_Ludovico_Fischer/server.php
@@ -0,0 +1,170 @@
+<?php
+
+/*
+Copyright 2010 Ludovico Fischer. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list
+   of conditions and the following disclaimer in the documentation and/or other materials
+   provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+class Server {
+    /* The array key works as id and is used in the URL
+       to identify the resource.
+    */
+    private $contacts = array('jim' => array('address' => '356 Trailer Park Avenue', 'url' => '/clients/jim'),
+                              'anne' => array('address' => '6321 Tycoon Road', 'url'=> '/clients/anne')
+);
+
+    public function serve() {
+      
+        $uri = $_SERVER['REQUEST_URI'];
+        $method = $_SERVER['REQUEST_METHOD'];
+        $paths = explode('/', $this->paths($uri));
+        array_shift($paths); // Hack; get rid of initials empty string
+        $resource = array_shift($paths);
+      
+        if ($resource == 'clients') {
+            $name = array_shift($paths);
+	
+            if (empty($name)) {
+                $this->handle_base($method);
+            } else {
+                $this->handle_name($method, $name);
+            }
+          
+        } else {
+            // We only handle resources under 'clients'
+            header('HTTP/1.1 404 Not Found');
+        } 
+    }
+        
+    private function handle_base($method) {
+        switch($method) {
+        case 'GET':
+            $this->result();
+            break;
+        default:
+            header('HTTP/1.1 405 Method Not Allowed');
+            header('Allow: GET');
+            break;
+        }
+    }
+
+    private function handle_name($method, $name) {
+        switch($method) {
+        case 'PUT':
+            $this->create_contact($name);
+            break;
+
+        case 'DELETE':
+            $this->delete_contact($name);
+            break;
+      
+        case 'GET':
+            $this->display_contact($name);
+            break;
+
+        default:
+            header('HTTP/1.1 405 Method Not Allowed');
+            header('Allow: GET, PUT, DELETE');
+            break;
+        }
+    }
+
+    private function create_contact($name){
+        if (isset($this->contacts[$name])) {
+            header('HTTP/1.1 409 Conflict');
+            return;
+        }
+        /* PUT requests need to be handled
+         * by reading from standard input.
+         */
+        $data = json_decode(file_get_contents('php://input'));
+        if (is_null($data)) {
+            header('HTTP/1.1 400 Bad Request');
+            $this->result();
+            return;
+        }
+        $this->contacts[$name] = $data; 
+        $this->result();
+    }
+    
+    private function delete_contact($name) {
+        if (isset($this->contacts[$name])) {
+            unset($this->contacts[$name]);
+            $this->result();
+        } else {
+            header('HTTP/1.1 404 Not Found');
+        }
+    }
+    
+    private function display_contact($name) {
+        if (array_key_exists($name, $this->contacts)) {
+            echo json_encode($this->contacts[$name]);
+        } else {
+            header('HTTP/1.1 404 Not Found');
+        }
+    }
+    
+    private function paths($url) {
+        $uri = parse_url($url);
+        return $uri['path'];
+    }
+    
+    /**
+     * Displays a list of all contacts.
+     */
+    private function result() {
+        header('Content-type: application/json');
+        echo json_encode($this->contacts);
+    }
+  }
+
+$server = new Server;
+$server->serve();
+
+/*
+  testing:
+  tutorial site:
+  code.tutsplus.com/tutorials/a-beginners-guide-to-http-and-rest--net-16340
+
+- spawn a php server which take a folder "code"
+php -S localhost:8282 -t code
+
+- retrieve current client
+curl -v http://localhost:8282/clients/
+  output: {"jim":{"address":"356 Trailer Park Avenue","url":"\/clients\/jim"},"anne":{"address":"6321 Tycoon Road","url":"\/clients\/anne"}}
+
+- add a client
+curl -v -X PUT http://localhost:8282/clients/paul -d '{"address":"Sunset Bay", "url":"\/clients\/paul"}'
+  output: {"jim":{"address":"356 Trailer Park Avenue","url":"\/clients\/jim"},"anne":{"address":"6321 Tycoon Road","url":"\/clients\/anne"},"paul":{"address":"Sunset Bay","url":"\/clients\/paul"}}
+
+- delete a client
+curl -v -X DELETE http://localhost:8282/clients/anne
+  output: {"jim":{"address":"356 Trailer Park Avenue","url":"\/clients\/jim"}}
+
+- try to put an existing client (get response 409)
+curl -v -X PUT http://localhost:8282/clients/anne
+
+- try to access non-existence client (get 404)
+curl -v http://localhost:8282/clients/jerry
+ * */
+
+?>
+
